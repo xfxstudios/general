a3f121cf81d16f98f78fd21f66ef468bf92d2ace
Carlos Quintero <info.fxstudios@gmail.com>
2018-04-01 00:52:50 -0400

    Inicio de Proyecto

---
+++ src/GeneralClass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace hitcel\general;
+
+class GeneralClass
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function muestra($phrase)
+    {
+        return $phrase;
+    }
+}
