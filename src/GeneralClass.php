b777343a79f9e91fc13d6213b19f571c34cfefe0
Carlos Quintero <info.fxstudios@gmail.com>
2018-04-03 10:23:23 -0400

    Agregada funcion de numeros a letras

--- src/GeneralClass.php
+++ src/GeneralClass.php
@@ -519,27 +519,194 @@ class GeneralClass
 	public function getMonedas($moneda = null){
         
         $data = array(
-            "euro"                    => (object) array("nombre"=>"Euro","iso"=>"EUR","isocode"=>"978","simboloDerecha"=>"","simboloIzquierda"=>"E","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"euros"),
-            "peso argentino"          => (object) array("nombre"=>"Peso Argentino","iso"=>"ARS","isocode"=>"032","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
-            "dolar australiano"       => (object) array("nombre"=>"Dólar Australiano","iso"=>"AUD","isocode"=>"036","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"dolares"),
-            "boliviano"               => (object) array("nombre"=>"Boliviano","iso"=>"BOB","isocode"=>"068","simboloDerecha"=>"","simboloIzquierda"=>"Bs","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"bolivianos"),
-            "dolar americano"         => (object) array("nombre"=>"Dólar Americano","iso"=>"USD","isocode"=>"840","simboloDerecha"=>"","simboloIzquierda"=>"us$","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"dolares"),
-            "real brasileño"          => (object) array("nombre"=>"Real Brasileño","iso"=>"BRL","isocode"=>"986","simboloDerecha"=>"","simboloIzquierda"=>"R$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"reales"),
-            "rupia india"             => (object) array("nombre"=>"Rupia","iso"=>"INR","isocode"=>"356","simboloDerecha"=>"","simboloIzquierda"=>"Rs","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"rupias"),
-            "dolar canadiense"        => (object) array("nombre"=>"Dólar Canadiense","iso"=>"CAD","isocode"=>"124","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"dolares"),
-            "peso chileno"            => (object) array("nombre"=>"Peso Chileno","iso"=>"CLP","isocode"=>"152","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
-            "yuan"                    => (object) array("nombre"=>"Yuan","iso"=>"CNY","isocode"=>"156","simboloDerecha"=>"","simboloIzquierda"=>"¥","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"yuanes"),
-            "peso colombiano"         => (object) array("nombre"=>"Peso Colombiano","iso"=>"COP","isocode"=>"170","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>"","separadormiles"=>"","plural"=>"pesos"),
-            "peso mexicano"         => (object) array("nombre"=>"Peso Mexicano","iso"=>"MXN","isocode"=>"4217","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>"","separadormiles"=>"","plural"=>"pesos"),
-            "peso cubano"             => (object) array("nombre"=>"Peso Cubano","iso"=>"CUP","isocode"=>"192","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
-            "libra esterlina"         => (object) array("nombre"=>"Libra Esterlina","iso"=>"GBP","isocode"=>"826","simboloDerecha"=>"","simboloIzquierda"=>"£","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"libras"),
-            "yen"                     => (object) array("nombre"=>"Yen","iso"=>"JPY","isocode"=>"392","simboloDerecha"=>"","simboloIzquierda"=>"¥","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"yenes"),
-            "sol"                     => (object) array("nombre"=>"Sol Peruano","iso"=>"PEN","isocode"=>"604","simboloDerecha"=>"","simboloIzquierda"=>"S/","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"soles"),
-            "bolívar"                 => (object) array("nombre"=>"Bólivar","iso"=>"VEF","isocode"=>"937","simboloDerecha"=>"Bs.","simboloIzquierda"=>"","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"bolivares"),
+            "euro"             => (object) array("nombre"=>"Euro","iso"=>"EUR","isocode"=>"978","simboloDerecha"=>"","simboloIzquierda"=>"E","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"euros"),
+            "pesoargentino"    => (object) array("nombre"=>"Peso Argentino","iso"=>"ARS","isocode"=>"032","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
+            "dolaraustraliano" => (object) array("nombre"=>"Dólar Australiano","iso"=>"AUD","isocode"=>"036","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"dolares"),
+            "boliviano"        => (object) array("nombre"=>"Boliviano","iso"=>"BOB","isocode"=>"068","simboloDerecha"=>"","simboloIzquierda"=>"Bs","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"bolivianos"),
+            "dolaramericano"   => (object) array("nombre"=>"Dólar Americano","iso"=>"USD","isocode"=>"840","simboloDerecha"=>"","simboloIzquierda"=>"us$","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"dolares"),
+            "realbrasileño"    => (object) array("nombre"=>"Real Brasileño","iso"=>"BRL","isocode"=>"986","simboloDerecha"=>"","simboloIzquierda"=>"R$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"reales"),
+            "rupiaindia"       => (object) array("nombre"=>"Rupia","iso"=>"INR","isocode"=>"356","simboloDerecha"=>"","simboloIzquierda"=>"Rs","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"rupias"),
+            "dolarcanadiense"  => (object) array("nombre"=>"Dólar Canadiense","iso"=>"CAD","isocode"=>"124","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"dolares"),
+            "pesochileno"      => (object) array("nombre"=>"Peso Chileno","iso"=>"CLP","isocode"=>"152","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
+            "yuan"             => (object) array("nombre"=>"Yuan","iso"=>"CNY","isocode"=>"156","simboloDerecha"=>"","simboloIzquierda"=>"¥","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"yuanes"),
+            "pesocolombiano"   => (object) array("nombre"=>"Peso Colombiano","iso"=>"COP","isocode"=>"170","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>"","separadormiles"=>"","plural"=>"pesos"),
+            "pesomexicano"     => (object) array("nombre"=>"Peso Mexicano","iso"=>"MXN","isocode"=>"4217","simboloDerecha"=>"","simboloIzquierda"=>"$ ","decimales"=>"2","separadordecimal"=>"","separadormiles"=>"","plural"=>"pesos"),
+            "pesocubano"       => (object) array("nombre"=>"Peso Cubano","iso"=>"CUP","isocode"=>"192","simboloDerecha"=>"","simboloIzquierda"=>"$","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"pesos"),
+            "libraesterlina"   => (object) array("nombre"=>"Libra Esterlina","iso"=>"GBP","isocode"=>"826","simboloDerecha"=>"","simboloIzquierda"=>"£","decimales"=>"2","separadordecimal"=>".","separadormiles"=>",","plural"=>"libras"),
+            "yen"              => (object) array("nombre"=>"Yen","iso"=>"JPY","isocode"=>"392","simboloDerecha"=>"","simboloIzquierda"=>"¥","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"yenes"),
+            "sol"              => (object) array("nombre"=>"Sol Peruano","iso"=>"PEN","isocode"=>"604","simboloDerecha"=>"","simboloIzquierda"=>"S/","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"soles"),
+            "bolívar"          => (object) array("nombre"=>"Bólivar","iso"=>"VEF","isocode"=>"937","simboloDerecha"=>"Bs.","simboloIzquierda"=>"","decimales"=>"2","separadordecimal"=>",","separadormiles"=>".","plural"=>"bolivares"),
         );
 
         return ($moneda == null) ? $data : $data[$moneda];
-    }
+	}
+	
+
+	//Retorna una cantidad en letras
+	//------    CONVERTIR NUMEROS A LETRAS         ---------------
+	//------    Máxima cifra soportada: 18 dígitos con 2 decimales
+	//------    999,999,999,999,999,999.99
+	// NOVECIENTOS NOVENTA Y NUEVE MIL NOVECIENTOS NOVENTA Y NUEVE BILLONES
+	// NOVECIENTOS NOVENTA Y NUEVE MIL NOVECIENTOS NOVENTA Y NUEVE MILLONES
+	// NOVECIENTOS NOVENTA Y NUEVE MIL NOVECIENTOS NOVENTA Y NUEVE 99/100 M.N.
+	public function numletras($xcifra){
+		$xarray = array(0 => "Cero",
+			1 => "UN", "DOS", "TRES", "CUATRO", "CINCO", "SEIS", "SIETE", "OCHO", "NUEVE",
+			"DIEZ", "ONCE", "DOCE", "TRECE", "CATORCE", "QUINCE", "DIECISEIS", "DIECISIETE", "DIECIOCHO", "DIECINUEVE",
+			"VEINTI", 30 => "TREINTA", 40 => "CUARENTA", 50 => "CINCUENTA", 60 => "SESENTA", 70 => "SETENTA", 80 => "OCHENTA", 90 => "NOVENTA",
+			100 => "CIENTO", 200 => "DOSCIENTOS", 300 => "TRESCIENTOS", 400 => "CUATROCIENTOS", 500 => "QUINIENTOS", 600 => "SEISCIENTOS", 700 => "SETECIENTOS", 800 => "OCHOCIENTOS", 900 => "NOVECIENTOS"
+		);
+	//
+		$xcifra = trim($xcifra);
+		$xlength = strlen($xcifra);
+		$xpos_punto = strpos($xcifra, ".");
+		$xaux_int = $xcifra;
+		$xdecimales = "00";
+		if (!($xpos_punto === false)) {
+			if ($xpos_punto == 0) {
+				$xcifra = "0" . $xcifra;
+				$xpos_punto = strpos($xcifra, ".");
+			}
+			$xaux_int = substr($xcifra, 0, $xpos_punto); // obtengo el entero de la cifra a covertir
+			$xdecimales = substr($xcifra . "00", $xpos_punto + 1, 2); // obtengo los valores decimales
+		}
+
+		$XAUX = str_pad($xaux_int, 18, " ", STR_PAD_LEFT); // ajusto la longitud de la cifra, para que sea divisible por centenas de miles (grupos de 6)
+		$xcadena = "";
+		for ($xz = 0; $xz < 3; $xz++) {
+			$xaux = substr($XAUX, $xz * 6, 6);
+			$xi = 0;
+			$xlimite = 6; // inicializo el contador de centenas xi y establezco el límite a 6 dígitos en la parte entera
+			$xexit = true; // bandera para controlar el ciclo del While
+			while ($xexit) {
+				if ($xi == $xlimite) { // si ya llegó al límite máximo de enteros
+					break; // termina el ciclo
+				}
+
+				$x3digitos = ($xlimite - $xi) * -1; // comienzo con los tres primeros digitos de la cifra, comenzando por la izquierda
+				$xaux = substr($xaux, $x3digitos, abs($x3digitos)); // obtengo la centena (los tres dígitos)
+				for ($xy = 1; $xy < 4; $xy++) { // ciclo para revisar centenas, decenas y unidades, en ese orden
+					switch ($xy) {
+						case 1: // checa las centenas
+							if (substr($xaux, 0, 3) < 100) { // si el grupo de tres dígitos es menor a una centena ( < 99) no hace nada y pasa a revisar las decenas
+								
+							} else {
+								$key = (int) substr($xaux, 0, 3);
+								if (TRUE === array_key_exists($key, $xarray)){  // busco si la centena es número redondo (100, 200, 300, 400, etc..)
+									$xseek = $xarray[$key];
+									$xsub = $this->subfijo($xaux); // devuelve el subfijo correspondiente (Millón, Millones, Mil o nada)
+									if (substr($xaux, 0, 3) == 100)
+										$xcadena = " " . $xcadena . " CIEN " . $xsub;
+									else
+										$xcadena = " " . $xcadena . " " . $xseek . " " . $xsub;
+									$xy = 3; // la centena fue redonda, entonces termino el ciclo del for y ya no reviso decenas ni unidades
+								}
+								else { // entra aquí si la centena no fue numero redondo (101, 253, 120, 980, etc.)
+									$key = (int) substr($xaux, 0, 1) * 100;
+									$xseek = $xarray[$key]; // toma el primer caracter de la centena y lo multiplica por cien y lo busca en el arreglo (para que busque 100,200,300, etc)
+									$xcadena = " " . $xcadena . " " . $xseek;
+								} // ENDIF ($xseek)
+							} // ENDIF (substr($xaux, 0, 3) < 100)
+							break;
+						case 2: // checa las decenas (con la misma lógica que las centenas)
+							if (substr($xaux, 1, 2) < 10) {
+								
+							} else {
+								$key = (int) substr($xaux, 1, 2);
+								if (TRUE === array_key_exists($key, $xarray)) {
+									$xseek = $xarray[$key];
+									$xsub = $this->subfijo($xaux);
+									if (substr($xaux, 1, 2) == 20)
+										$xcadena = " " . $xcadena . " VEINTE " . $xsub;
+									else
+										$xcadena = " " . $xcadena . " " . $xseek . " " . $xsub;
+									$xy = 3;
+								}
+								else {
+									$key = (int) substr($xaux, 1, 1) * 10;
+									$xseek = $xarray[$key];
+									if (20 == substr($xaux, 1, 1) * 10)
+										$xcadena = " " . $xcadena . " " . $xseek;
+									else
+										$xcadena = " " . $xcadena . " " . $xseek . " Y ";
+								} // ENDIF ($xseek)
+							} // ENDIF (substr($xaux, 1, 2) < 10)
+							break;
+						case 3: // checa las unidades
+							if (substr($xaux, 2, 1) < 1) { // si la unidad es cero, ya no hace nada
+								
+							} else {
+								$key = (int) substr($xaux, 2, 1);
+								$xseek = $xarray[$key]; // obtengo directamente el valor de la unidad (del uno al nueve)
+								$xsub = $this->subfijo($xaux);
+								$xcadena = " " . $xcadena . " " . $xseek . " " . $xsub;
+							} // ENDIF (substr($xaux, 2, 1) < 1)
+							break;
+					} // END SWITCH
+				} // END FOR
+				$xi = $xi + 3;
+			} // ENDDO
+
+			if (substr(trim($xcadena), -5, 5) == "ILLON") // si la cadena obtenida termina en MILLON o BILLON, entonces le agrega al final la conjuncion DE
+				$xcadena.= " DE";
+
+			if (substr(trim($xcadena), -7, 7) == "ILLONES") // si la cadena obtenida en MILLONES o BILLONES, entoncea le agrega al final la conjuncion DE
+				$xcadena.= " DE";
+
+			// ----------- esta línea la puedes cambiar de acuerdo a tus necesidades o a tu país -------
+			if (trim($xaux) != "") {
+				switch ($xz) {
+					case 0:
+						if (trim(substr($XAUX, $xz * 6, 6)) == "1")
+							$xcadena.= "UN BILLON ";
+						else
+							$xcadena.= " BILLONES ";
+						break;
+					case 1:
+						if (trim(substr($XAUX, $xz * 6, 6)) == "1")
+							$xcadena.= "UN MILLON ";
+						else
+							$xcadena.= " MILLONES ";
+						break;
+					case 2:
+						if ($xcifra < 1) {
+							$xcadena = "CERO $xdecimales/100 M.N.";
+						}
+						if ($xcifra >= 1 && $xcifra < 2) {
+							$xcadena = "UN $xdecimales/100 M.N. ";
+						}
+						if ($xcifra >= 2) {
+							$xcadena.= " $xdecimales/100 M.N. "; //
+						}
+						break;
+				} // endswitch ($xz)
+			} // ENDIF (trim($xaux) != "")
+			// ------------------      en este caso, para México se usa esta leyenda     ----------------
+			$xcadena = str_replace("VEINTI ", "VEINTI", $xcadena); // quito el espacio para el VEINTI, para que quede: VEINTICUATRO, VEINTIUN, VEINTIDOS, etc
+			$xcadena = str_replace("  ", " ", $xcadena); // quito espacios dobles
+			$xcadena = str_replace("UN UN", "UN", $xcadena); // quito la duplicidad
+			$xcadena = str_replace("  ", " ", $xcadena); // quito espacios dobles
+			$xcadena = str_replace("BILLON DE MILLONES", "BILLON DE", $xcadena); // corrigo la leyenda
+			$xcadena = str_replace("BILLONES DE MILLONES", "BILLONES DE", $xcadena); // corrigo la leyenda
+			$xcadena = str_replace("DE UN", "UN", $xcadena); // corrigo la leyenda
+		} // ENDFOR ($xz)
+		return trim($xcadena);
+	}
+
+	// END FUNCTION
+
+	public function subfijo($xx)
+	{ // esta función regresa un subfijo para la cifra
+		$xx = trim($xx);
+		$xstrlen = strlen($xx);
+		if ($xstrlen == 1 || $xstrlen == 2 || $xstrlen == 3)
+			$xsub = "";
+		//
+		if ($xstrlen == 4 || $xstrlen == 5 || $xstrlen == 6)
+			$xsub = "MIL";
+		//
+		return $xsub;
+	}
 
 
 	/**
