cdcd4a318d5dff2eacca772ce2540d6c1a2fb9b1
Carlos Quintero <info.fxstudios@gmail.com>
2018-05-01 14:37:46 -0400

    Agregada funcion de eliminacion de simbolos

--- src/GeneralClass.php
+++ src/GeneralClass.php
@@ -114,7 +114,7 @@ class GeneralClass
         curl_close($curl);
 
         return $return;
-    }
+	}
 	public function getCNE($X){
 		//Obtenemos el HTML del CNE
 			try{
@@ -194,6 +194,64 @@ class GeneralClass
 		$claveB = $hash;
 		return $claveB;
 	}//END
+
+
+
+	//Limpia una cadena de simbolos injnecesarios
+	public function eliminar_simbolos($string){
+ 
+        $string = trim($string);
+     
+        $string = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $string
+        );
+     
+        $string = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $string
+        );
+     
+        $string = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $string
+        );
+     
+        $string = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $string
+        );
+     
+        $string = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $string
+        );
+     
+        $string = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C',),
+            $string
+        );
+     
+        $string = str_replace(
+            array("\\", "¨", "º", "-", "~",
+                 "#", "@", "|", "!", "\"",
+                 "·", "$", "%", "&", "/",
+                 "(", ")", "?", "'", "¡",
+                 "¿", "[", "^", "<code>", "]",
+                 "+", "}", "{", "¨", "´",
+                 ">", "< ", ";", ",", ":",
+                 ".", " "),
+            ' ',
+            $string
+        );
+        return $string;
+    } 
 
 	//Retorna Informacion completa de una IP
 	public function city($X=null){
