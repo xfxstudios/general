6ddfc46e5741b81758fabb8bfb97c1b08c8aed29
Carlos Quintero <info.fxstudios@gmail.com>
2018-05-26 21:32:36 -0400

    update

--- src/Myemail.php
+++ src/Myemail.php
@@ -36,14 +36,70 @@ class Myemail
         $this->mj = new \Mailjet\Client($this->apiKey, $this->apiSecret,true,['version' => $this->ini['versionMail']]);
     }
 
-    public function from($X){
+    public function from($X=null){
+        if($X==null){
+            throw new Emailexception("No se han enviado parámetros de Envío", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos Enviados no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<3){
+            throw new Emailexception("Faltan Parámetros de envío", 1);
+            exit;
+        }
+        if(empty($X[0])){
+            throw new Emailexception("Falta el Email destinatario", 1);
+            exit;
+        }
+        if(empty($X[1])){
+            throw new Emailexception("Falta el Nombre del Destinatario", 1);
+            exit;
+        }
+        if(empty($X[2])){
+            throw new Emailexception("Falta el Asunto del Email", 1);
+            exit;
+        }
         $this->from     = $X[0];
         $this->fromName = $X[1];
         $this->subject  = $X[2];
         return $this;
     }
 
-    public function for($X){
+    public function for($X=null){
+        if(empty($this->from) || empty($this->fromName) || empty($this->subject)){
+            throw new Emailexception("Faltan o están incompletos los Parámetros de Envío", 1);
+            exit;
+        }
+        if($X==null){
+            throw new Emailexception("Faltán parámetros del Receptor", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos del receptor no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<4){
+            throw new Emailexception("Faltán parámetros del Receptor", 1);
+            exit;
+        }
+        if(empty($X[0])){
+            throw new Emailexception("Falta el Email del Receptor", 1);
+            exit;
+        }
+        if(empty($X[1])){
+            throw new Emailexception("Falta el nombre del Receptor del Email", 1);
+            exit;
+        }
+        if(empty($X[2])){
+            throw new Emailexception("Falta la Información a ser enviada en el Email", 1);
+            exit;
+        }
+        if(empty($X[3])){
+            throw new Emailexception("Falta la información en texto plano", 1);
+            exit;
+        }
         $this->for       = $X[0];
         $this->forName   = $X[1];
         $this->info      = $X[2];
@@ -51,27 +107,107 @@ class Myemail
         return $this;
     }
 
-    public function copy($X){
+    public function copy($X=null){
+        if($X==null){
+            throw new Emailexception("Faltán parámetros de la Copia", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos de copia no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<3){
+            throw new Emailexception("Faltán parámetros de la Copia", 1);
+            exit;
+        }
+        if($X[0] && empty($X[1])){
+            throw new Emailexception("Falta el Email del receptor de la Copia", 1);
+            exit;
+        }
+        if($X[0] && empty($X[2])){
+            throw new Emailexception("Falta el Nombre del receptor de la Copia", 1);
+            exit;
+        }
         $this->copy     = $X[0];
         $this->copyMail = $X[1];
         $this->copyName = $X[2];
         return $this;
     }
     
-    public function hiddenCopy($X){
+    public function hiddenCopy($X=null){
+        if($X==null){
+            throw new Emailexception("Faltán parámetros de la Copia Oculta", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos de copia oculta no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<3){
+            throw new Emailexception("Faltán parámetros de la Copia Oculta", 1);
+            exit;
+        }
+        if($X[0] && empty($X[1])){
+            throw new Emailexception("Falta el Email del receptor de la Copia Oculta", 1);
+            exit;
+        }
+        if($X[0] && empty($X[2])){
+            throw new Emailexception("Falta el Nombre del receptor de la Copia Oculta", 1);
+            exit;
+        }
         $this->hiddenCopy     = $X[0];
         $this->hiddenCopyMail = $X[1];
         $this->hiddenCopyName = $X[2];
         return $this;
     }
 
-    public function template($X){
+    public function template($X=null){
+        if($X==null){
+            throw new Emailexception("Faltán parámetros de la Plantilla", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos de la Plantilla no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<3){
+            throw new Emailexception("Faltán parámetros de la Plantilla", 1);
+            exit;
+        }
+        if($X[0] && empty($X[1])){
+            throw new Emailexception("Falta el Nombre del Archivo HTMl/HTM de la Plantilla", 1);
+            exit;
+        }
         $this->template     = $X[0];
         $this->templateName = $X[1];
         return $this;
     }
     
-    public function attachFile($X){
+    public function attachFile($X=null){
+        if($X==null){
+            throw new Emailexception("Faltán parámetros del Adjunto", 1);
+            exit;
+        }
+        if(!is_array($X)){
+            throw new Emailexception("Los datos del adjunto no son un arreglo válido", 1);
+            exit;
+        }
+        if(count($X)<4){
+            throw new Emailexception("Faltán parámetros del Adjunto", 1);
+            exit;
+        }
+        if($X[0] && empty($X[1])){
+            throw new Emailexception("Falta el Tipo de Adjunto a Enviar", 1);
+            exit;
+        }
+        if($X[0] && empty($X[2])){
+            throw new Emailexception("Falta el nuevo Nombre del Adjunto a Enviar", 1);
+            exit;
+        }
+        if($X[0] && empty($X[3])){
+            throw new Emailexception("Falta la Codificación Base64 del Archivo a Enviar", 1);
+            exit;
+        }
         $this->attachment = $X[0];
         $this->cType       = $this->contentType($X[1]);
         $this->fileName    = $X[2];
@@ -126,6 +262,11 @@ class Myemail
             case 'csv':
                 return 'text/csv';
             break;
+
+            default:
+                throw new Emailexception("El tipo de Archivo a Enviar, no se encuentra registrado", 1);
+                exit;
+            break;
         }
 
     }
@@ -162,9 +303,21 @@ class Myemail
     }
 
     public function send(){
+        if(empty($this->from) || empty($this->fromName) || empty($this->subject)){
+            throw new Emailexception("Faltan o están incompletos los Parámetros de Envío", 1);
+            exit;
+        }
+        if(empty($this->for) || empty($this->forName) || empty($this->info) || empty($this->plaiText)){
+            throw new Emailexception("Faltan o están incompletos los Parámetros del Receptor del Envío", 1);
+            exit;
+        }
         $data = null;
         if($this->template){
             $file = file_get_contents(APPPATH.'/plantillas/'.$this->templateName);
+            if(!$file){
+                throw new Emailexception("No se ha podido recuperar el archivo ".$this->templateName, 1);
+                exit;
+            }
             $file = str_replace("%info%",$this->info,$file);
             $data = $file;
         }else{
@@ -193,26 +346,7 @@ class Myemail
         ];
         
         $response = $this->mj->post(Resources::$Email, ['body' => $body]);
-        if ($response->success())
-            return $response->getData();
-        else
-            //return $response->getStatus();
-            return $response->getData();
+        return $response->getData();
     }//
 
-    public function ex(){
-        $v = 0;
-
-        try{
-            if($v==0){
-                throw new Emailesception("Error Processing Request", 1);
-            }
-        }catch(Emailexception $e){
-            return $e->getMessage();
-        }
-        
-
-    }
-
 }
-
