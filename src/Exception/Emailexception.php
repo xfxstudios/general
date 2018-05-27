acf8b74a83ee16f61d3dd9e4ea73d29dbf9415ea
Carlos Quintero <info.fxstudios@gmail.com>
2018-05-26 20:32:40 -0400

    update

--- src/Exception/Emailexception.php
+++ src/Exception/Emailexception.php
@@ -2,10 +2,11 @@
 
 namespace xfxstudios\Exception;
 
-class Emailexception extends Exception
+class Emailexception extends \Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null) {
-        parent::__construct($message, $code, $previous);
+    public function __construct($message = null, $code = 0)
+    {
+        parent::__construct($message, $code);
     }
 
     public function __toString() {
